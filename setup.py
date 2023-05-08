5f5fe46b621e2667226dfb6a79ff8fd5f22b92b4
Danderson123 <danielanderson1@hotmail.com>
2023-05-08 12:06:00 +0100

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='approximate_cluster_identities',
-    version='0.1',
+    version='0.1.1',
     description='A package to calculate and visualise approximate cluster identities for a large number of short nucleotide sequences using minimisers.',
     long_description=long_description,
     long_description_content_type='text/markdown',
